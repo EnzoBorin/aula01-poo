d4d93f7649c8e04c80cf5e53cae4c2a2c17a123d
EnzoBorin <enzo.borin@aluno.senai.br>
2024-08-15 11:31:34 -0300

    Aula 01 POO - Criando e testando classe

---
+++ dist/testePessoa.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//Importar a classe que quero utilizar
+const Pessoa_1 = require("./Pessoa");
+// Instanciar (criar objeto)
+const pessoa1 = new Pessoa_1.Pessoa('Felisberto', 25);
+const pessoa2 = new Pessoa_1.Pessoa('Maria', 21);
+console.log(pessoa1.getNome());
+console.log(pessoa2.getIdade());
+//# sourceMappingURL=testePessoa.js.map
---
+++ src/Pessoa.ts
@@ -0,0 +1,48 @@
+export class Pessoa {
+
+    //ATRIBUTOS
+    private nome: string;
+    private idade: number;
+
+    //CONSTRUCTOR
+    public constructor(_nome: string, _idade: number) {
+        this.nome = _nome;
+        this.idade = _idade;
+
+    }
+
+    //MÉTODOS GET E SET
+    public getNome(): string {
+        return this.nome;
+       
+    }
+
+    public getIdade(): number {
+        return this.idade;
+       
+    }
+
+    public setNome(_nome: string): void {
+        this.nome = _nome;
+        
+    }
+
+    public setIdade(_idade: number): void {
+        this.idade = _idade;
+        
+    }
+
+    // MÉTODOS DIAGRAMA
+    public idadePessoa(): number {
+        return this.idade;
+    }
+
+    public aniversario(): number {
+        this.idade++;
+        return this.idade;
+    }
+        
+    public correr(): void {
+        console.log(`${this.nome} está correndo... RUN ${this.nome} RUN`)
+    }
+}
